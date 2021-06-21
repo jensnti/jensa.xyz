2d9b0aeb26388a62a9080f8578266451584c948e
jensnti <jensandreasson77@gmail.com>
2021-06-21 21:14:19 +0200

    💩✨🚧images

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ const glob = require('fast-glob');
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const fs = require('fs');
 
+// const imageShortcode = require('./src/shortcodes/image');
+
 // Import transforms
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 const parseTransform = require('./src/transforms/parse-transform.js');
@@ -19,6 +21,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget('./src/scss/');
     eleventyConfig.addWatchTarget('./src/js/');
 
+    eleventyConfig.addPassthroughCopy('src/robots.txt');
     eleventyConfig.addPassthroughCopy('src/images');
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
@@ -34,10 +37,23 @@ module.exports = function (eleventyConfig) {
     // Shortcodes
     glob.sync(['src/shortcodes/*.js']).forEach((file) => {
         let shortcodes = require('./' + file);
-        Object.keys(shortcodes).forEach((name) =>
-            eleventyConfig.addShortcode(name, shortcodes[name])
-        );
+        Object.keys(shortcodes).forEach((name) => {
+            if (name === 'image') {
+                eleventyConfig.addNunjucksAsyncShortcode(
+                    name,
+                    shortcodes[name]
+                );
+                eleventyConfig.addLiquidShortcode(name, shortcodes[name]);
+                eleventyConfig.addJavaScriptFunction(name, shortcodes[name]);
+            } else {
+                eleventyConfig.addShortcode(name, shortcodes[name]);
+            }
+        });
     });
+
+    // eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+    // eleventyConfig.addLiquidShortcode('image', imageShortcode);
+    // eleventyConfig.addJavaScriptFunction('image', imageShortcode);
 
     // PairedShortcodes
     glob.sync(['src/paired-shortcodes/*.js']).forEach((file) => {
@@ -61,7 +77,9 @@ module.exports = function (eleventyConfig) {
     );
 
     eleventyConfig.addCollection('feed', (collectionApi) =>
-        [...collectionApi.getFilteredByGlob('src/posts/*.md')].reverse().slice(0, 5)
+        [...collectionApi.getFilteredByGlob('src/posts/*.md')]
+            .reverse()
+            .slice(0, 5)
     );
 
     const markdownLibrary = markdownIt({
@@ -82,6 +100,21 @@ module.exports = function (eleventyConfig) {
         });
 
     eleventyConfig.setLibrary('md', markdownLibrary);
+
+    // 404
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, browserSync) {
+                const content_404 = fs.readFileSync('public/404.html');
+
+                browserSync.addMiddleware('*', (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
 
     return {
         dir: {
