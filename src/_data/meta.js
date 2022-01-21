653e6a99af58b62e7e80bd0be4166758c1cebf37
Jens Andreasson <jensandreasson77@gmail.com>
2022-01-21 14:47:31 +0100

    💬 meta data

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,6 @@
 module.exports = {
     siteName: 'J.Anteckningar',
     siteDesc: 'Blog och anteckningsblock för Jens Andreasson',
-    colors: {
-        fg: '#fff',
-        bg: '#121212',
-    },
     author: {
         name: 'Jens Andreasson',
         email: 'jensandreasson77@gmail.com',
@@ -13,4 +9,6 @@ module.exports = {
     url: process.env.URL || 'http://localhost:8080',
     time: Date.now(),
     locale: 'sv-SE',
+    lang: 'sv',
+    theme: '#2d303c',
 };
