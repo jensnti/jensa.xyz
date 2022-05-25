543c6aa396a37d26ceda563bcf783cbc00dbb868
Jens Andreasson <jens.andreasson@ntig.se>
2022-05-25 11:56:10 +0200

    💄 nging

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+    eleventyConfig.addPassthroughCopy('./src/assets/');
 
     eleventyConfig.addPassthroughCopy('./src/images/jens.jpg');
 
